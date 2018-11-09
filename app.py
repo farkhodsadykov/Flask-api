7d405131e5bef91f7e13ed3f037de603740599e9
farkhodsadykov <sadykovfarkhod@gmail.com>
2018-11-09 16:38:53 -0600

    Example

--- app.py
+++ app.py
@@ -3,72 +3,93 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, request, jsonify, make_response
 from flask_wtf import FlaskForm, RecaptchaField
 from flask_sqlalchemy  import SQLAlchemy
+from functools import wraps
 from uuid import uuid4
 import uuid
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DataBase/example.db'
+app.config['SECRET_KEY'] = '57644AC4CDAFC87E54731C8D249DE'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flaskuser:flask**@159.89.180.31/test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+def login_required_api(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify({'message' : 'Token is missing'})
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message': 'Tokeb is Invalid'}), 401
+        return f(current_user, *args, **kwargs)
+    return decorated
+
+
+db = SQLAlchemy(app)
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.String(50), unique=True)
-    first_name = db.Column(db.String(15))
-    last_name = db.Column(db.String(15))
-    user_name = db.Column(db.String(15), unique=True)
+    public_id = db.Column(db.String(60), unique=True)
+    username = db.Column(db.String(15), unique=True)
     password = db.Column(db.String(80))
-    email = db.Column(db.String(15))
+    firstname = db.Column(db.String(20))
+    lastname = db.Column(db.String(20))
+    email = db.Column(db.String(30))
+    status = db.Column(db.String(10))
+    role = db.Column(db.String(10))
+    group = db.Column(db.String(20))
 
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-
-    users = User.query.all()
-
-    return users
-@app.route('/user/<public_id>', methods=['GET', 'POST'])
-def user_info(public_id):
     data = request.get_json()
-    if data:
-        try:
-            user = {}
-            user['username'] =
-            user['public_id'] = str(uuid.uuid4())
-            user['first_name'] =
-            user['last_name'] =
-            user['email'] =
-            user['password'] = hashed_password
-            return jsonify({'User informations': user})
+    return jsonify({'message': data})
+    # return jsonify({ 'Meassage': 'Mising data' })
 
-        except ValueError as error:
-            return jsonify({'message': error})
-    return jsonify({'message' : "missing public_id"})
 
-@app.route('/signup', methods=['POST', 'GET'])
-def signup():
-    data = request.get_json()
-    if not data:
-        return jsonify({'message': 'missing username'})
-    hashed_password = generate_password_hash(data['password'], method='sha256')
-    if data:
-        new_user = User(public_id=str(uuid.uuid4()), user_name=data['username'], first_name=data['firstname'], last_name=data['lastname'], email=data['email'])
-@app.route('/api/signup', methods = ['POST'])
+
+# Quering single user
+@app.route('/user/<public_id>', methods=['GET'])
+@login_required_api
+def get_one_user(current_user, public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+    if not user:
+        return jsonify({"message": "No user found!" })
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['firstname'] = user.firstname
+    user_data['lastname'] = user.lastname
+    user_data['email'] = user.email
+    user_data['username'] = user.username
+    user_data['role'] = user.role
+    user_data['group'] = user.group
+    return jsonify({'User': user_data})
+
+
+# Creating user
+@app.route('/signup', methods=['GET', 'POST'])
 def new_user():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    if username is None or password is None:
-        abort(400) # missing arguments
-    if User.query.filter_by(username = username).first() is not None:
-        abort(400) # existing user
-    user = User(username = username)
-    user.hash_password(password)
-    db.session.add(user)
-    db.session.commit()
-    return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
-
+    data = request.get_json()
+    try:
+        hashed_password = generate_password_hash(data['password'], method='sha256')
+        new_user = User(public_id=str(uuid.uuid4()), password=hashed_password, username=data['username'], firstname=data['firstname'], lastname=data['lastname'],  email=data['email'], status=True, role='student', group='default')
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'User has been created'})
+    except Exception as e:
+        return jsonify({'message': 'User already created'})
+    return jsonify({ 'message': 'Mising data' })
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080, host='0.0.0.0')
+    app.run(debug=True, port=8081, host='0.0.0.0')
